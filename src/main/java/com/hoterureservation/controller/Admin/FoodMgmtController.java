37e4969e05778b6ce45dd9874fc5e0b8d03cb64f
phutnps13223 <phutranngoc8@gmail.com>
2021-10-27 16:38:46 +0700

    pageable_Food

--- src/main/java/com/hoterureservation/controller/Admin/FoodMgmtController.java
+++ src/main/java/com/hoterureservation/controller/Admin/FoodMgmtController.java
@@ -1,27 +1,31 @@
 package com.hoterureservation.controller.Admin;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.hoterureservation.entities.Food;
 import com.hoterureservation.repositories.FoodRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FoodMgmtController {
     @Autowired
     FoodRepository foodRepository;
 
-    @RequestMapping("/Food")
-    public String show(Model model) {
+    @RequestMapping("/Food/page")
+    public String show(Model model, @RequestParam("p") Optional<Integer> p) {
         // load Food
-        Food item = new Food();
-        model.addAttribute("item", item);
-        List<Food> FoodItems = foodRepository.findAll();
+        Pageable pageable = PageRequest.of(p.orElse(0), 5);
+        Page<Food> FoodItems = foodRepository.findAll(pageable);
         model.addAttribute("List", FoodItems);
         return "Admin/Food";
     }
