6368301e156ba7a0e5b68d8cfc15fef82ac4edb2
NganMapTran <ngandhlps13187@fpt.edu.vn>
2021-11-26 20:44:02 +0700

    API export and chart

--- src/main/java/com/hoterureservation/repositories/ServiceRepository.java
+++ src/main/java/com/hoterureservation/repositories/ServiceRepository.java
@@ -1,5 +1,7 @@
 package com.hoterureservation.repositories;
 
+import java.util.List;
+
 import com.hoterureservation.entities.Services;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface ServiceRepository extends JpaRepository<Services, Long>{
   @Query("Select count(s) from Services s")
   public int countService();
+
+  public List<Services> findByNameLike(String key);
 }
